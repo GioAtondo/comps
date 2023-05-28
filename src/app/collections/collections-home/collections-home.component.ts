f743edae3892af5287ef14f543105f7b687c034d
GioAtondo <56622385+GioAtondo@users.noreply.github.com>
2023-05-28 14:50:24 -0700

    Communicating Down Table Data

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
   styleUrls: ['./collections-home.component.css']
 })
-export class CollectionsHomeComponent {
+export class CollectionsHomeComponent implements OnInit{
+  data = [
+    { name: 'James', age: 24, job: 'Designer'},
+    { name: 'Jill', age: 26, job: 'Engineer'},
+    { name: 'Elyse', age: 25, job: 'Engineer'}
+  ];
+
+  header = [
+    { key: 'name', label: 'Name'},
+    { key: 'age', label: 'Name'},
+    { key: 'job', label: 'Name'}
+  ];
+
+  constructor() {}
+  ngOnInit(){}
 
 }
