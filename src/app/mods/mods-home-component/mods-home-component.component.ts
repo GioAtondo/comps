c6bf04180b66f625ce6d4693911fe127ca6810ab
Giovanni Alberto Salas <gioatondo7@gmail.com>
2023-05-29 10:31:17 -0700

    opening Modal with button

--- src/app/mods/mods-home-component/mods-home-component.component.ts
+++ src/app/mods/mods-home-component/mods-home-component.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mods-home-component',
   templateUrl: './mods-home-component.component.html',
   styleUrls: ['./mods-home-component.component.css']
 })
-export class ModsHomeComponentComponent {
+export class ModsHomeComponentComponent implements OnInit{
+  modalOpen = false;
+  constructor(){
 
+  }
+
+  ngOnInit(){
+
+  }
+  onClick(){
+    this.modalOpen = !this.modalOpen;
+  }
 }
